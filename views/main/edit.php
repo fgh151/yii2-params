da876e6dba375dfe0f7b6374887be255a58cf7b9
Fedor B Gorsky <fedor@support-pc.org>
2016-03-24 15:01:36 +0300

    Fix namespace error

--- views/main/edit.php
+++ views/main/edit.php
@@ -2,19 +2,19 @@
 
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
-use common\modules\params\Module;
+use \fgh151\modules\params\Module;
 
-/* @var $model common\modules\params\models\ParamsModel */
+/* @var $model \fgh151\modules\params\models\ParamsModel */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
 <div class="app_params_settings-main-index">
     <h1><?= $model->paramsFilePath ?></h1>
     <?php $form = ActiveForm::begin();
-        foreach ($model->attributes() as $attribute):
-            echo $form->field($model,$attribute);
-        endforeach;
-        echo Html::submitButton(Module::t('app','Save settings'),['class'=>'btn btn-success']);
+    foreach ($model->attributes() as $attribute):
+        echo $form->field($model,$attribute);
+    endforeach;
+    echo Html::submitButton(Module::t('app','Save settings'),['class'=>'btn btn-success']);
     ?>
     <?php ActiveForm::end()?>
 </div>
